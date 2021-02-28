6e24b8a20954c47a359f491a2a3eff90d85fb118
Ugongjae <turtle0191@naver.com>
2021-02-28 22:05:13 +0900

    medium

--- Longest_Palindromic_Substring.cpp
+++ Longest_Palindromic_Substring.cpp
@@ -1,51 +1,33 @@
 #include <iostream>
 #include <string>
-#include <vector>
 using namespace std;
 
 class Solution {
 public:
-    vector<int> getPan(string s,int l,int r){
+    int getPan(string s,int l,int r){
         while(l>=0&&r<s.size()&&s[l]==s[r]){
             l--;
             r++;
         }
-        vector<int> res;
-        res.push_back(l+1);
-        res.push_back(r-1);
-        res.push_back(r-l-1);
-        return res;
+        return r-l-1;
     }
     string longestPalindrome(string s) {
         int tmp=-1;
-        int ltmp=-1;
-        int rtmp=-1;
+        int start=0;
         if(s.size()==1){
             return s;
         }
         for(int i=0;i<s.size()-1;i++){
-            vector<int> len1=getPan(s,i,i);
-            vector<int> len2=getPan(s,i,i+1);
-            if(len2[2]>len1[2]){
-                if(len2[2]>tmp){
-                    ltmp=len2[0];
-                    rtmp=len2[1];
-                    tmp=len2[2];
-                }
-            }else{
-                if(len1[2]>tmp){
-                    ltmp=len1[0];
-                    rtmp=len1[1];
-                    tmp=len1[2];
-                }
+            int len1=getPan(s,i,i);
+            int len2=getPan(s,i,i+1);
+            int tmpmax=max(len1,len2);
+            if(tmpmax>tmp){
+                tmp=tmpmax;
+                start=i-(tmpmax-1)/2;
             }
         }
-        vector<int> len3=getPan(s,s.size(),s.size());
-        if(len3[2]>tmp){
-            return s.substr(len3[0],len3[2]);
-        }
 
-        return s.substr(ltmp,rtmp-ltmp+1);
+        return s.substr(start,tmp);
     }
 };
 
