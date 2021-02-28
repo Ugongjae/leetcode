b1cd194a2d4a4811d7babf1d9491aa5b7fcb6051
Ugongjae <turtle0191@naver.com>
2021-02-28 22:48:52 +0900

    medium

---
+++ ZigZag_Conversion.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+private:
+    string arr[1001];
+public:
+    string convert(string s, int numRows) {
+
+        int dir=1;
+        int j=1;
+        for(int i=0;i<s.size();i++){
+            arr[j].push_back(s[i]);
+            j+=dir;
+            if(j==numRows){
+                dir=-1;
+            }
+            if(j==1){
+                dir=1;
+            }
+        }
+        string res;
+        for(int i=1;i<=numRows;i++){
+            for(int j=0;j<arr[i].size();j++){
+                res.push_back(arr[i][j]);
+            }
+        }
+
+        return res;
+    }
+};
+
+int main(){
+    Solution s;
+    cout<<s.convert("AB",1)<<endl;
+
+
+    return 0;
+}
