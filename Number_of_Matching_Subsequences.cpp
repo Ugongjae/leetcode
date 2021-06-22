dc8f6bfe8e7ac486f9313d336157a0e6f512ff64
Ugongjae <turtle0191@naver.com>
2021-06-22 21:29:46 +0900

    June Week4 1st

---
+++ Number_of_Matching_Subsequences.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        unordered_map<string, bool> m;
+        int res = 0;
+        for(auto word : words){
+            if(m.find(word) != m.end()){res += m[word]; continue;}
+            int i=0, j=0;
+            while(i<word.length() && j<s.length()){
+                if(s[j] == word[i])i++,j++;
+                else j++;
+            }
+            m[word] = i==word.length();
+            res += i==word.length();
+        }
+        return res;
+    }
+};
+
+int main(){
+
+    return 0;
+}
